2e6a871f2339d741f87ae5ae598ee2f63fe7f0b8
FooBuTT <foobu@mail.ru>
2023-06-08 12:30:41 +0300

    item

--- client/src/Components/UI/sideBarCategory.tsx
+++ client/src/Components/UI/sideBarCategory.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { Button,Container } from 'reactstrap';
+import React from 'react';
+import { Button, Container, List } from 'reactstrap';
+import { useAppDispatch, useAppSelector } from '../Redux/hooks';
 export default function sideBarCategory(): JSX.Element {
+
+  const dispatch = useAppDispatch();
+  const allCategory = useAppSelector((store) => store);
+
   return (
     <Container>
-  
-
+      {allCategory.map((category) => (
+        <List>
+          <li>{category.name}</li>
+        </List>
+      ))}
     </Container>
-  )
+  );
 }
