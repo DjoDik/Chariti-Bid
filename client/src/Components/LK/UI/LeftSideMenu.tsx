e5fa489eb3d190e5ea8401d0843e0724c1ed86b7
Anna <a_karvatskaya@mail.ru>
2023-06-12 17:28:00 +0300

    0/0/10

--- client/src/Components/LK/UI/LeftSideMenu.tsx
+++ client/src/Components/LK/UI/LeftSideMenu.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListItem, ListItemButton } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../Redux/hooks';
 import { Link } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { closeModal, openModal } from '../../Redux/slice/modalSlice';
 import LkMainPage from './AddItemModal';
+
 type Props = {
   children?: JSX.Element;
   redirect: string;
   isAllowed: boolean;
 };
-
 export default function LeftSideMenu({}: Props) {
   const userId = useAppSelector((store) => store.user);
   const { id } = userId;
@@ -29,38 +29,23 @@ export default function LeftSideMenu({}: Props) {
     <Box
       sx={{
         position: 'absolute',
-        width: '500px',
-        height: '300px',
-        bottom: '-650px',
-        right: 0,
-        zIndex: 1,
-        backgroundColor: '#DFF0D8',
-        boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.2)',
-        mt: 1,
+        width: '300px',
+        height: '250px',
+        top: '100%',
+        backgroundColor: '#white',
+        mt: 5,
       }}
     >
-      <ListGroup>
-        <ListGroupItem
-          tag={Link}
-          to="/cabinet/basket"
-          action
-          style={{ backgroundColor: 'transparent' }}
-        >
-          Корзина
-        </ListGroupItem>
-        <ListGroupItem onClick={() => handleOpenModal()} style={{ backgroundColor: 'transparent' }}>
-          <LkMainPage />
-          Добавить товар
-        </ListGroupItem>
-        <ListGroupItem
-          tag={Link}
-          to={`/useritem/${id}`}
-          action
-          style={{ backgroundColor: 'transparent' }}
-        >
-          Мои товары
-        </ListGroupItem>
-      </ListGroup>
+      <ListItemButton component={Link} to="/basket">
+        Корзина
+      </ListItemButton>
+      <ListItemButton button onClick={() => handleOpenModal()}>
+        <LkMainPage />
+        Добавить товар
+      </ListItemButton>
+      <ListItemButton component={Link} to={`/useritem/${id}`}>
+        Мои товары
+      </ListItemButton>
     </Box>
   );
 }
